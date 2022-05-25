463461745eb21a47f3d01ccec826ef5227ab13ac
“binghan” <binghan60@gmail.com>
2022-05-25 21:34:12 +0800

    新增資料輸入表單

--- proj/ab-add.php
+++ proj/ab-add.php
@@ -11,22 +11,38 @@ $title = '牛奶的網站 - 新增通訊錄資料';
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">新增資料</h5>
-                    <form>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <form name="form1">
+                        <div class="mb-3" onsubmit="return false;">
+                            <label for="name" class="form-label">* name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                            <div class="form-text"></div>
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                        <form name="form1">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">email</label>
+                                <input type="email" class="form-control" id="email" name="email">
+                                <div class="form-text"></div>
+                            </div>
+                            <form name="form1">
+                                <div class="mb-3">
+                                    <label for="mobile" class="form-label">mobile</label>
+                                    <input type="number" class="form-control" id="mobile" name="mobile" pattern="09\d{8}">
+                                    <div class="form-text"></div>
+                                </div>
+                                <form name="form1">
+                                    <div class="mb-3">
+                                        <label for="birthday" class="form-label">birthday</label>
+                                        <input type="date" class="form-control" id="birthday" name="birthday">
+                                        <div class="form-text"></div>
+                                    </div>
+                                    <form name="form1">
+                                        <div class="mb-3">
+                                            <label for="address" class="form-label">address</label>
+                                            <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                                            <div class="formtext"></div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
                 </div>
             </div>
         </div>
