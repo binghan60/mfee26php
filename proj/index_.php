1047870133811ddee99a7805205224b50120ecce
“binghan” <binghan60@gmail.com>
2022-05-25 19:33:21 +0800

    pdo-quote

--- proj/index_.php
+++ proj/index_.php
@@ -1,8 +1,10 @@
 <?php require __DIR__ . '/parts/connect_db.php' ?>
-
-
-
 <?php include __DIR__ . '/parts/html-head.php' ?>
 <?php include __DIR__ . '/parts/navbar.php' ?>
+<div class="container">
+    <h2>Home</h2>
+    <p><?= $pdo->quote("Alice's cats") ?></p>
+    <!-- quote標引號 用來跳脫SQL裡的單引號 避免SQL injection隱碼攻擊 -->
+</div>
 <?php include __DIR__ . '/parts/scripts.php' ?>
 <?php include __DIR__ . '/parts/html-foot.php' ?>
