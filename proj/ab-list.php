58e8fbcf5728703a268a7e028bbc5a2b2699cea8
“binghan” <binghan60@gmail.com>
2022-05-25 20:56:57 +0800

    delete edit icon

--- proj/ab-list.php
+++ proj/ab-list.php
@@ -25,7 +25,7 @@ if ($totalRows > 0) {
         exit;
     }
 
-    $sql = sprintf("SELECT * FROM address_book LIMIT %s ,%s", ($page - 1) * $perPage, $perPage);
+    $sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s ,%s", ($page - 1) * $perPage, $perPage);
     $rows = $pdo->query($sql)->fetchAll();
 }
 
@@ -79,23 +79,35 @@ if ($totalRows > 0) {
     <table class="table table-success table-striped">
         <thead>
             <tr>
+                <th scope="col">
+                    <i class="fa-solid fa-trash-can"></i>
+                </th>
                 <th scope="col">#</th>
                 <th scope="col">姓名</th>
                 <th scope="col">手機</th>
                 <th scope="col">電話</th>
                 <th scope="col">生日</th>
                 <th scope="col">地址</th>
+                <th scope="col">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($rows as $r) : ?>
                 <tr>
+                    <td>
+                        <a href="#"><i class="fa-solid fa-trash-can"></i></a>
+                    </td>
                     <td><?= $r["sid"] ?></td>
                     <td><?= $r["name"] ?></td>
                     <td><?= $r["mobile"] ?></td>
                     <td><?= $r["email"] ?></td>
                     <td><?= $r["birthday"] ?></td>
                     <td><?= $r["address"] ?></td>
+                    <td>
+                        <a href=""><i class="fa-solid fa-pen-to-square"></i></a>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
