3f4a3b18c6a284d47866e1648ecd5bd075bec01d
“binghan” <binghan60@gmail.com>
2022-05-26 08:04:38 +0800

    前端檢查

--- proj/ab-add.php
+++ proj/ab-add.php
@@ -50,14 +50,45 @@ $title = '牛奶的網站 - 新增通訊錄資料';
 </div>
 <?php include __DIR__ . '/parts/scripts.php' ?>
 <script>
+    const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
+    const mobile_re = /^09\d{2}-?\d{3}-?\d{3}$/;
+
+    const name_f = document.form1.name;
+    const email_f = document.form1.email;
+    const mobile_f = document.form1.mobile;
+
+
+
     async function sendData() {
-        const fd = new FormData(document.form1)
+        // TODO: 欄位檢查, 前端的檢查
+        let isPass = true; // 預設是通過檢查的
+
+        if (name_f.value.length < 2) {
+            alert('姓名至少兩個字');
+            isPass = false;
+        }
+        if (email_f.value && !email_re.test(email_f.value)) {
+            alert('email 格式錯誤');
+            isPass = false;
+        }
+        if (mobile_f.value && !mobile_re.test(mobile_f.value)) {
+            alert('手機號碼格式錯誤');
+            isPass = false;
+        }
+        // 沒有通過就結束函式,不再往下
+        if (!isPass) {
+            return; // 結束函式
+        }
+
+        const fd = new FormData(document.form1);
         const r = await fetch('ab-add-api.php', {
             method: 'POST',
-            body: fd
+            body: fd,
         });
         const result = await r.json();
-        console.log(result)
+        console.log(result);
+
+
     }
 </script>
 <?php include __DIR__ . '/parts/html-foot.php' ?>
